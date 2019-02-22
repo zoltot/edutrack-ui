c4bdbb477aa69f6223f4a3711f95e1c5b3ac6b37
Cristian Garcia <2940cristian@gmail.com>
2019-02-22 13:00:51 -0800

    Airbnb linter (#13)
    * Testing random example of eslint rules
    
    * Added eslint airbnb rules
    
    * Fixed travisci from not reporting to slack
    
    * Changed package.json dependencies and eslintrc

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = exports = {
+    "env": {
+        "es6": true,
+        "browser": true
+    },
+
+    "extends": ["react-app", "airbnb"],
+
+    "rules": {
+        "eol-last": "off",
+    }
+};
